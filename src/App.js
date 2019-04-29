ed35bfc2e382eb8a1fc73048405338267c240d0e
Seil Cho <seilcho7@hotmail.com>
2019-04-29 11:11:14 -0400

    adds axios for single character

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// To do Ajax in a React app:
+// 0. optional: install axios
+// 1. you need a class component
+// 2. add ajax requests (componentDidMount is the "earliest")
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      character: {} // By default, set your state to an empty
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios.get("https://anapioficeandfire.com/api/characters/583");
+    console.log(response.data);
+    this.setState({
+      character: response.data
+    }, () => {
+      console.log('done setting state');
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <li>name: {this.state.character.name}</li>
+        <li>born: {this.state.character.born}</li>
+        <li>culture: {this.state.character.culture}</li>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
